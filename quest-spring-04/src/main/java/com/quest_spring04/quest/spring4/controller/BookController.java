8636e35348394c7398879185f93e6b4ee536fe90
julien <julienmulle@gmail.com>
2022-08-02 09:19:34 +0200

    first commit

--- quest-spring-04/src/main/java/com/quest_spring04/quest/spring4/controller/BookController.java
+++ quest-spring-04/src/main/java/com/quest_spring04/quest/spring4/controller/BookController.java
@@ -2,13 +2,20 @@ package com.quest_spring04.quest.spring4.controller;
 
 import com.quest_spring04.quest.spring4.entity.Book;
 import com.quest_spring04.quest.spring4.repository.BookRepository;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 import java.util.Map;
-
+@RestController
 public class BookController {
     @Autowired
     BookRepository bookRepository;
@@ -18,36 +25,30 @@ public class BookController {
         return bookRepository.findAll();
     }
 
-    @GetMapping("/books/{id}"){
-        public Book show(@PathVariable("id") int id){
-            return bookRepository.findById(id).get();
-        }
+    @GetMapping("/books/{id}")
+        public Book show(@PathVariable int id) {
+        return bookRepository.findById(id).get();
     }
 
-    @PostMapping("/books/search"){
-        public List<Book> search(@RequestBody Map<String> body){
+    @PostMapping("/books/search")
+        public List<Book> search(@RequestBody Map<String, String> body){
             String searchTerm = body.get("text");
-            return bookRepository.findByTitleContainOrContentContaining(searchTerm);
+            return bookRepository.findByTitleContainOrDescriptionContaining(searchTerm, searchTerm);
         }
-    }
 
-    @PostMapping("/books"){
+    @PostMapping("/books")
         public Book create(@RequestBody Book book ){
-            String title = body.get("title");
-            String author = body.get("author");
-            String description = body.get("description");
             return bookRepository.save(book);
         }
-    }
 
-    @PutMapping("/books/{id}"){
+    @PutMapping("/books/{id}")
         public Book update(@PathVariable("id") int id, @RequestBody Book book){
             Book bookToUpdate = bookRepository.findById(id).get();
             bookToUpdate.setTitle(book.getTitle());
             bookToUpdate.setAuthor(book.getAuthor());
             bookToUpdate.setDescription(book.getDescription());
+            return bookRepository.save(bookToUpdate);
         }
-    }
 
     @DeleteMapping("books/{id}")
     public boolean delete(@PathVariable int id){
--- quest-spring-04/src/main/java/com/quest_spring04/quest/spring4/repository/BookRepository.java
+++ quest-spring-04/src/main/java/com/quest_spring04/quest/spring4/repository/BookRepository.java
@@ -1,10 +1,10 @@
 package com.quest_spring04.quest.spring4.repository;
 
 import com.quest_spring04.quest.spring4.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
-public class BookRepository {
-    public List<Book> findByTitleContainOrContentContaining(String text, String texAgain) {
-        return null;
-    }
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    public List<Book> findByTitleContainOrDescriptionContaining(String text, String texAgain);
 }
